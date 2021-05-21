53826e0280dee93ebf7c0a6d45afc3c3de4b2ff4
hahunavth <vuthanhha.2001@gmail.com>
2021-05-21 20:26:25 +0700

    Update .gitignore

--- inc/utility.h
+++ inc/utility.h
@@ -1,15 +1,15 @@
 #ifndef UTILITY_H_
 #define UTILITY_H_
 
-typedef enum PhonebookStrErr
+enum PhoneBookErr
 {
 	STR_OK,
 	LEN_EQUAL_ZERO,
 	FIRST_IS_SPACE,
 	END_IS_SPACE,
 	NUMBER_10,
-	NUMBER_FIRST_0
-}PhonebookStrErr;
+	NUMBER_FIRST_ERR	//K có + hoặc 0 ở đầu sđt
+};
 
 extern int validate_name(char name[]);
 
